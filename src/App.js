6b47d0d61cb4c5358466ccdc411954bce07ee8bc
wreck888 <DChui88@gmail.com>
2021-12-08 17:20:13 -0600

    api photo finished

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
 
 function App() {
+  const [nasaData, setNasaData] = useState([]);
+
+
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then(response => {
+        // console.log(response.data.url)
+        setNasaData(response.data.url)
+      })
+  }, [])
+
+
   return (
     <div className="App">
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        Photo of the Day! <span role="img" aria-label='go!'>🚀</span>!
       </p>
+      <img src={nasaData}></img>
     </div>
   );
 }
